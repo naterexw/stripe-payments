d8e510435bd801d399364bfb53b54b27b326c20b
Nate Welling <nate@natewelling.com>
2016-04-08 11:25:15 +0700

    passing params from form to stripe controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,8 +5,6 @@ class ChargesController < ApplicationController
 
   def create
     # Amount in cents
-    @amount = 500
-
     customer = Stripe::Customer.create(
       :email => params[:stripeEmail],
       :source  => params[:stripeToken]
@@ -14,8 +12,8 @@ class ChargesController < ApplicationController
 
     charge = Stripe::Charge.create(
       :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
+      :amount      => params[:amount],
+      :description => 'Growth Hacking Crash Course',
       :currency    => 'usd'
     )
 
